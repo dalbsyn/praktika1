59cc08edd3c5f4702dd218e727e18d3f3b6245fe
dalbsyn <rmrfmanwoman@gmail.com>
2025-05-27 13:00:02 +0500

    Исправление описания ссылки на базу данных
    В предыдущем варианте ссылка ранее описывалась с помощью встроенного класса URL в sqlalchemy, но alembic и, вроде, сам sqlalchemy жаловались на пароль, который был точно правильным.
    
    Сделал через форматирование строк, пока что.

--- core/settings.py
+++ core/settings.py
@@ -1,3 +1,4 @@
+from sqlalchemy import URL
 from dotenv import load_dotenv
 from os import getenv
 
@@ -8,3 +9,13 @@ class Settings:
     db_host = getenv('DB_HOST')
     db_port = getenv('DB_PORT')
     db_name = getenv('DB_NAME')
+    '''
+    В параметр port передается значение типа string, но вообще переменная
+    ожидает типа integer, отчего, как минимум, LSP в VSCode показывает ошибку. 
+    Ожидается, что пользователь в .env-файле точно введет значение нужного 
+    порта правильно, то есть только целым числом. 
+    Иначе - интерпретатор все равно выплюнет ошибку о неправильном типе.
+    В общем, игнорируйте ошибку.
+    '''
+    
+    url = 'postgresql+psycopg://{0}:{1}@{2}:{3}/{4}'.format(db_user, db_password, db_host, db_port, db_name)
