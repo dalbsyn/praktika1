c952e7b54ded6e04a80d2362161a6b2daaf68d04
dalbsyn <rmrfmanwoman@gmail.com>
2025-05-30 15:49:52 +0500

    Изменения во входных данных к таблицам
    Затронуты таблицы cards и transactions.
    
    Изменение заключается в том, чтобы они принимали непосредственно значаение временной метки с помощью модуля datetime, а не строки.

--- app/database/cards.py
+++ app/database/cards.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import TIMESTAMP, VARCHAR, ForeignKey, DATE
 from app.database.base import Base
+import datetime
 
 
 class Cards(Base):
@@ -9,5 +10,5 @@ class Cards(Base):
     expiry_date: Mapped[str] = mapped_column(DATE, nullable=False)
     cvc: Mapped[str] = mapped_column(VARCHAR(4), nullable=False)
     cardholder_name: Mapped[str] = mapped_column(VARCHAR(128), nullable=False)
-    issue_date: Mapped[str] = mapped_column(TIMESTAMP)
+    issue_date: Mapped[datetime.date] = mapped_column(DATE)
     card_status: Mapped[str] = mapped_column(VARCHAR(32), nullable=False, default='active')
--- app/database/transactions.py
+++ app/database/transactions.py
@@ -1,12 +1,13 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import TIMESTAMP, VARCHAR, UUID, DECIMAL, ForeignKey, TEXT
 from app.database.base import Base
+import datetime
 
 class Transactions(Base):
     transaction_id: Mapped[str] = mapped_column(UUID, unique=True)
     account_id: Mapped[int] = mapped_column(ForeignKey('accounts.id', ondelete='restrict'), nullable=False)
     transaction_type: Mapped[str] = mapped_column(VARCHAR(64), nullable=False)
-    transaction_date: Mapped[str] = mapped_column(TIMESTAMP, nullable=False)
+    transaction_date: Mapped[datetime.datetime] = mapped_column(TIMESTAMP, nullable=False)
     amount: Mapped[float] = mapped_column(DECIMAL(15, 2), nullable=False)
     description: Mapped[str] = mapped_column(TEXT)
     transaction_status: Mapped[str] = mapped_column(VARCHAR(64), nullable=False)
